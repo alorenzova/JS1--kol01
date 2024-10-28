a287817ea5c4562881b3296ef62021c4b2ff84b9
Aneta Lorenzová <aneta.lorenzova@gmail.com>
2024-10-29 00:51:26 +0100

    ukol

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+Úkol 01
+FIT e-mail
+-Pokud se přihlásíte jako student například na Fakultu Informačních Technologí ČVUT, bude vám informačním systémem automaticky přidělen e-mail, 
+- který se vytvoří z vašeho jména a příjmení. 
+- Z vašeho příjmení se vezme pět prvních znaků 
+- a z vašeho křestního jména první tři. 
+- Výsledek se převede na malá písmena a připojí se doména fit.cvut.cz. Pro jméno Jana Procházková tak vznikne adresa prochjan@fit.cvut.cz.
+
+Vytvořte webovou stránku, 
+- která požádá uživatele nejdříve o jeho křestní jméno 
+- a pak příjmení, 
+- obojí bez diakritiky. 
+- Pomocí metody trim zajistěte, že ve jméně ani příjmení nebudou na začátku ani na konci žádné bílé znaky. 
+- Pomocí metody slice vyřízněte ze jména i příjmení příslušné části. 
+- Pomocí interpolace řetězců sestavte výslednou adresu a vypište ji do stránky.
+*/
+
+const firstName = prompt("Zadejte prosím vaše křestní jméno (bez diakritiky).").trim()
+const lastName = prompt("Zadejte prosím vaše příjmení bez (diakritiky).").trim()
+
+const firstPart = lastName.slice(0, 5).toLowerCase()
+const secondPart = firstName.slice(0, 3).toLowerCase()
+
+const email = `${firstPart}${secondPart}@fit.cvut.cz`
+
+document.body.innerHTML = "<p>vaše automaticky přidělěná e-mailová adrease: " + email + "</p>"
